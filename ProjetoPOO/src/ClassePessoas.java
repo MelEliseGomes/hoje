c7e5600519790bee9aacf051365f7f026d45d2a2
Mel Elise Gomes Cavalheiro <163855084+MelEliseGomes@users.noreply.github.com>
2025-07-31 12:30:26 -0300

    Add files via upload

---
+++ ProjetoPOO/src/ClassePessoas.java
@@ -0,0 +1,16 @@
+
+public class ClassePessoas {
+
+	// Atributos
+	String nomeAt;
+	String documentoAt;
+	float rendaAt;
+	
+	//Construtores: Quando criar um objeto obriga a colocar os parâmetros
+	public ClassePessoas(String nomePar, String documentoPar, float rendaPar) {
+		
+		this.nomeAt = nomePar;
+		this.documentoAt = documentoPar;
+		this.rendaAt = rendaPar;
+	}
+}
